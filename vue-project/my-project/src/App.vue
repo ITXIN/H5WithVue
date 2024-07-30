cff9cd9a5044ecb77488238d4f52e307a72c497d
apple1 <liyaqing_it@126.com>
2024-07-30 16:11:16 +0800

    重新初始化vue2项目

--- vue-project/my-project/src/App.vue
+++ vue-project/my-project/src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <HelloWorld/> -->
-    <Home/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld'
-import Home from './components/Home'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Home
+    HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- vue-project/src/llbridge.js
+++
@@ -1,5 +0,0 @@
-export class llbridge {
-  call(params) {
-      
-  }
-}
